f0fd192349021dbbd227b61dcb99a9dbcd6c81b8
noorulhudaajmal <68446582+noorulhudaajmal@users.noreply.github.com>
2022-05-04 00:43:32 +0500

    Initial commit - Adding initial packages

---
+++ src/Queue/QueueOverflowException.java
@@ -0,0 +1,13 @@
+package Queue;
+
+public class QueueOverflowException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public QueueOverflowException() {
+        super();
+    }
+
+    public QueueOverflowException(String message) {
+        super(message);
+    }
+}
---
+++ src/Queue/QueueUnderflowException.java
@@ -0,0 +1,13 @@
+package Queue;
+
+public class QueueUnderflowException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public QueueUnderflowException() {
+        super();
+    }
+
+    public QueueUnderflowException(String message) {
+        super(message);
+    }
+}
